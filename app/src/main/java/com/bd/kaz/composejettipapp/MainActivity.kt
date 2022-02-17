c0fea57a1b3002821ad56d77fd01f95012808935
Lelin Mahmud <lelin.cse@gmail.com>
2022-02-17 12:17:02 +0600

    top header seation done

--- app/src/main/java/com/bd/kaz/composejettipapp/MainActivity.kt
+++ app/src/main/java/com/bd/kaz/composejettipapp/MainActivity.kt
@@ -3,13 +3,23 @@ package com.bd.kaz.composejettipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bd.kaz.composejettipapp.ui.theme.ComposeJetTipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +29,34 @@ class MainActivity : ComponentActivity() {
             ComposeJetTipAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxWidth(),
+                    color = Color.White
                 ) {
-                    Greeting("Android")
+                   TopHeader()
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = false)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeJetTipAppTheme {
-        Greeting("Android")
+fun TopHeader(totalPerPerson :Double =0.0){
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(20.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val total ="%.2f".format(totalPerPerson)
+            Text(text = "Total Per Person", style = MaterialTheme.typography.h4)
+            Text(text = "$$total", style = MaterialTheme.typography.h4, fontWeight = FontWeight.ExtraBold)
+        }
     }
 }
