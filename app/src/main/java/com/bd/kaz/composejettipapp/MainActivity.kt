89eaa28f432911d59b71aa9fc353a8e1a7d031ef
Lelin Mahmud <lelinaits20@gmail.com>
2022-02-18 01:35:13 +0600

    Plus minus button added

--- app/src/main/java/com/bd/kaz/composejettipapp/MainActivity.kt
+++ app/src/main/java/com/bd/kaz/composejettipapp/MainActivity.kt
@@ -1,27 +1,38 @@
 package com.bd.kaz.composejettipapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bd.kaz.composejettipapp.components.InputField
 import com.bd.kaz.composejettipapp.ui.theme.ComposeJetTipAppTheme
+import com.bd.kaz.composejettipapp.widgets.RoundedIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +42,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxWidth(),
-                    color = Color.White
+                    color = MaterialTheme.colors.background
                 ) {
                    Column {
                        TopHeader()
@@ -44,21 +55,13 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun MainContent(){
-   Surface(
-       modifier = Modifier
-           .fillMaxWidth()
-           .padding(5.dp)
-           .height(200.dp),
-       shape = RoundedCornerShape(corner = CornerSize(8.dp)),
-       border = BorderStroke(width = 1.dp, color = Color.LightGray),
-       color = Color.White,
-       elevation = 2.dp
-   ) {
-
-   }
+    BillForm(){billAmount ->
+        Log.e("TAG", "MainContent: $billAmount" )
+    }
 }
 
 @Preview(showBackground = false)
@@ -79,6 +82,74 @@ fun TopHeader(totalPerPerson :Double =0.0){
             val total ="%.2f".format(totalPerPerson)
             Text(text = "Total Per Person", style = MaterialTheme.typography.h4)
             Text(text = "$$total", style = MaterialTheme.typography.h4, fontWeight = FontWeight.ExtraBold)
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(modifier: Modifier = Modifier,onValChange :(String) -> Unit ={}){
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .height(200.dp),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray),
+        elevation = 2.dp
+    ) {
+        Column(modifier = Modifier.padding(6.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start) {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled =true ,
+                isSingleLine = true,
+                onAction = KeyboardActions{
+                    if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
+                    keyboardController?.hide()
+                }
+            )
+            if (validState){
+                Row(
+                    modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start,
+                ) {
+                    Text(
+                        text = "Split",
+                        modifier = Modifier.align(
+                            alignment = Alignment.CenterVertically
+                        )
+
+                        )
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(modifier = Modifier.padding(horizontal = 3.dp),
+                    horizontalArrangement = Arrangement.End) {
+                        RoundedIconButton(imageVector = Icons.Default.Remove,
+                            onClick = { /*TODO*/ })
+                        Text(text = "1")
+                        RoundedIconButton(imageVector = Icons.Default.Add,
+                            onClick = { /*TODO*/ })
+                    }
+                }
+            }
+            else{
+                Box() {
+                    
+                }
+            }
         }
     }
 }
